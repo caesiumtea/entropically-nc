2e4ea5d314c211626d63af23ab8c9d50715d4092
caesiumtea <caesiumtea@gmail.com>
2025-02-26 08:18:56 -0500

    Add accessibility menu with cursor toggle

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,25 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
 });
+
+document.querySelector('#access-icon').addEventListener('click', () => {
+    document.querySelector('#access-menu').removeAttribute('hidden');
+});
+
+document.querySelector('#access-menu-close').addEventListener('click', () => {
+    document.querySelector('#access-menu').setAttribute('hidden', '');
+});
+
+// animateCursor = localStorage?.getItem("animate-cursor") ?? true;
+
+document.querySelector('#access-cursor').addEventListener('click', () => {
+    if (animateCursor) {
+        window.localStorage.setItem('animate-cursor', false);
+        window.location.reload();
+        // document.querySelector('#access-cursor').textContent = "enable cursor animation";
+    } else {
+        window.localStorage.setItem('animate-cursor', true);
+        window.location.reload();
+        // document.querySelector('#access-cursor').textContent = "disable cursor animation";
+    }
+})
