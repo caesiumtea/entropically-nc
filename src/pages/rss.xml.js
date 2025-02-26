3ee31b7c321282d7cf4069e538ced2231338e175
caesiumtea <caesiumtea@gmail.com>
2025-02-26 04:29:18 -0500

    Update RSS to include tutorials

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -16,3 +16,19 @@ export async function GET(context) {
         customData: `<language>en-us</language>`,
     });
 }
+
+export async function GET(context) {
+    const posts = await getCollection("tuts");
+    return rss({
+        title: 'entropically | tutorials',
+        description: "tutorials and tips for indie web creators",
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.id}/`,
+        })),
+        customData: `<language>en-us</language>`,
+    });
+}
