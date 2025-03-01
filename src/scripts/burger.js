92ac9b24b2868b3eba7fe9c2fbbb8fe716e9422d
caesiumtea <caesiumtea@gmail.com>
2025-03-01 06:10:25 -0500

    Redo nav to use details element

---
+++ src/scripts/burger.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    let burger = document.getElementById("burger");
+
+    burger.addEventListener("keydown", (event) => {
+        let key = event.code;
+        if (key == "Escape") {
+            burger.removeAttribute("open");
+        }
+    });
+};
