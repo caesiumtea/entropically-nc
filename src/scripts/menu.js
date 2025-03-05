eb8b0e2c043bc1248b6786cb170fd5ac0dc21879
caesiumtea <caesiumtea@gmail.com>
2025-03-05 12:18:22 -0500

    Fix code fonts in switcher

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -26,16 +26,16 @@ document.querySelector('#select-font').addEventListener('change', () => {
         window.localStorage.setItem('font', 'dyslexic');
         document.documentElement.style.setProperty('--font-main', 'OpenDyslexic, Arial, Helvetica, sans-serif ');
         document.documentElement.style.setProperty('--font-accent', 'OpenDyslexic, Arial, Helvetica, sans-serif');
-        document.documentElement.style.setProperty('--font-code', '"Courier New", Courier, monospace;');
+        document.documentElement.style.setProperty('--font-code', '"Iosevka Fixed", "Courier New", Courier, monospace');
     } else if (font === "pixel") {
         window.localStorage.setItem('font', 'pixel');
         document.documentElement.style.setProperty('--font-main', 'Monogram, Iosevka, Verdana, Geneva, Tahoma, sans-serif');
         document.documentElement.style.setProperty('--font-accent', 'Monogram, Eczar, Cambria, Cochin, Georgia, Times, "Times New Roman", serif');
-        document.documentElement.style.setProperty('--font-code', 'Monogram, Eczar, Cambria, Cochin, Georgia, Times, "Times New Roman", serif');
+        document.documentElement.style.setProperty('--font-code', 'Monogram, "Iosevka Fixed", "Courier New", Courier, monospace');
     } else {
         window.localStorage.setItem('font', 'default');
         document.documentElement.style.setProperty('--font-main', 'Iosevka, Verdana, Geneva, Tahoma, sans-serif');
         document.documentElement.style.setProperty('--font-accent', 'Eczar, Cambria, Cochin, Georgia, Times, "Times New Roman", serif');
-        document.documentElement.style.setProperty('--font-code', '"Courier New", Courier, monospace;');
+        document.documentElement.style.setProperty('--font-code', '"Iosevka Fixed", "Courier New", Courier, monospace');
     }
 });
