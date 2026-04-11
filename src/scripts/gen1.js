59d5f073a5d17aa39882420898f1f15a0c2f27f9
vance <caesiumtea@gmail.com>
2026-04-11 13:24:37 -0400

    Make circles!

--- src/scripts/gen1.js
+++ src/scripts/gen1.js
@@ -1,10 +1,21 @@
 export function runSketch() {
     const canvas = document.getElementById('gen1');
-    if (!canvas) return;
 
     const ctx = canvas.getContext('2d');
-    if (!ctx) return;
 
-    
+    for (let i = 0; i < 30; i++) {
+        const radius = 10 + Math.floor(Math.random() * 16)
+        const x = Math.floor(Math.random() * 400)
+        const y = Math.floor(Math.random() * 400)
+        const color_r = 255 - Math.floor(Math.random() * 100)
+        const color_g = 255 - Math.floor(Math.random() * 100)
+        const color_b = 255 - Math.floor(Math.random() * 100)
+        
+        ctx.fillStyle = `rgb(${color_r} ${color_g} ${color_b} / 66)`
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fill();
+        
+    }
     
 }
