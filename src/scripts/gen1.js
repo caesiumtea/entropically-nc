ea269f0c2d2a50d3bf7e3c3e3e91fb0e28e8bdc3
vance <caesiumtea@gmail.com>
2026-04-11 12:43:12 -0400

    Initialize canvas context

--- src/scripts/gen1.js
+++ src/scripts/gen1.js
@@ -1,5 +1,10 @@
 export function runSketch() {
     const canvas = document.getElementById('gen1');
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
     
     
 }
