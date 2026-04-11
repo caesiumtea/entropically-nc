d5aeaa0e02fc9ae5395fa3d6b6d57e041d19f639
vance <caesiumtea@gmail.com>
2026-04-11 15:51:51 -0400

    Tweak generation on gen1

--- src/scripts/gen1.js
+++ src/scripts/gen1.js
@@ -3,15 +3,15 @@ export function runSketch() {
 
     const ctx = canvas.getContext('2d');
 
-    for (let i = 0; i < 30; i++) {
-        const radius = 10 + Math.floor(Math.random() * 16)
+    for (let i = 0; i < 50; i++) {
+        const radius = 10 + Math.floor(Math.random() * 40)
         const x = Math.floor(Math.random() * 400)
         const y = Math.floor(Math.random() * 400)
         const color_r = 255 - Math.floor(Math.random() * 100)
         const color_g = 255 - Math.floor(Math.random() * 100)
         const color_b = 255 - Math.floor(Math.random() * 100)
         
-        ctx.fillStyle = `rgb(${color_r} ${color_g} ${color_b} / 66)`
+        ctx.fillStyle = `rgb(${color_r} ${color_g} ${color_b} / 66%)`
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.fill();
